481d9ac7ff7dd00fba866e97ae03a4407446268a
Muslimbek Olimov <olimovswe@gmail.com>
2026-09-15 11:03:17 +0500

    Begin

---
+++ Lab_01/11_begin.py
@@ -0,0 +1,12 @@
+a = float(input("Birinchi sonni kiriting : "))
+b = float(input("Ikkinchi sonni kiriting : "))
+
+yigindi = a + b
+kopaytma = a * b
+a_kvadrat = a ** 2
+b_kvadrat = b ** 2
+
+print(f"Yig'indisi: {yigindi}")
+print(f"Ko'paytmasi: {kopaytma}")
+print(f"Birinchi sonning kvadrati: {a_kvadrat}")
+print(f"Ikkinchi sonning kvadrati: {b_kvadrat}")
---
+++ Lab_01/12_begin.py
@@ -0,0 +1,13 @@
+
+a = float(input("Birinchi sonni kiriting (a != 0): "))
+b = float(input("Ikkinchi sonni kiriting (b != 0): "))
+
+yigindi = a + b
+kopaytma = a * b
+a_modul = abs(a)
+b_modul = abs(b)
+
+print(f"Yig'indisi: {yigindi}")
+print(f"Ko'paytmasi: {kopaytma}")
+print(f"Birinchi sonning moduli: {a_modul}")
+print(f"Ikkinchi sonning moduli: {b_modul}")
