a314e978f11465d465207a7b16a7931b95f3d167
Muslimbek Olimov <olimovswe@gmail.com>
2026-09-17 10:09:07 +0500

    if-else

---
+++ Lab_03/23_if.py
@@ -0,0 +1,11 @@
+
+print(f"Tizm 18 yosh yoki undan katta foydalanuvchilar uchun!")
+age  = int(input("Yoshingizni krting: "))
+
+if age >=  18:
+    print(f"Tizmga xush kelibsiz!")
+
+else:
+    print(f"Sizning yoshingiz {age}\n"
+          f"Voyaga yetgansiz!\n"
+          f"Tizmdan faqat 18 yosh yoki undan  kattalar foydalna oladi.")
